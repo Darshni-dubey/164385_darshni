9dc73788ff9110d0a9e6631b6581f7ce1b07e328
Darshni-dubey <44637645+Darshni-dubey@users.noreply.github.com>
2018-11-14 21:43:31 +0530

    Add files via upload

---
+++ ArraySorting.java
@@ -0,0 +1,29 @@
+package com.darshni.ds;
+import java.util.Arrays;
+public class ArraySorting {
+	
+	 
+	  
+	
+	    
+	    public static int findIndex(int arr[], int t) 
+	    { 
+	  
+	        int index = Arrays.binarySearch(arr, t); 
+	        return (index < 0) ? -1 : index; 
+	    } 
+	    
+	    public static void main(String[] args) 
+	    { 
+	        int[] my_array = { 1, 2, 3, 4, 5, 6, 7 }; 
+	  
+	      
+	        System.out.println("Index position of 5 is: "
+	                           + findIndex(my_array, 5)); 
+	  
+	      
+	    } 
+	} 
+
+
+
